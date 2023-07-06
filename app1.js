bc330bc598811512ab28f8e62ea6c6ca1d15f794
Aman241999 <amansinghania453>
2023-07-06 11:51:22 +0530

    hello

--- app1.js
+++ app1.js
@@ -1,3 +1,10 @@
 let list=document.getElementsByClassName("list-group-item");
 list[1].style.backgroundColor="green";
 list[2].style.display="none";
+let second=document.querySelectorAll(".list-group-item:nth-child(2)");
+second[1].style.color="red";
+let odd=document.querySelectorAll("li:nth-child(odd)");
+for(let i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor="green";
+}
