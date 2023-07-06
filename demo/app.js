dc523ab55bfcc79970f1e542cbb77f660598713e
Aman241999 <amansinghania453>
2023-07-06 08:37:27 +0530

    helloworld

--- demo/app.js
+++ demo/app.js
@@ -1,4 +1,4 @@
-let items=document.getElementsByClassName("list-group-item");
+let items=document.getElementsByTagName("li");
 items[2].style.backgroundColor='green';
 for(let i=0;i<items.length;i++){
     items[i].style.fontWeight="bold";
